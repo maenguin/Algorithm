096bb6e98ec6ca430a570571b17927b8b9cdcbaa
maenguin <maenguin924@gmail.com>
2020-12-10 15:09:55 +0900

    programmers 서울에서김서방찾기 solved

---
+++ src/main/java/programmers/level1/서울에서김서방찾기.java
@@ -0,0 +1,22 @@
+package programmers.level1;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class 서울에서김서방찾기 {
+
+    public String solution(String[] seoul) {
+        String answer = "";
+
+        for (int i = 0; i < seoul.length; i++) {
+            if (seoul[i].equals("Kim")){
+                answer = "김서방은 "+i+"에 있다";
+                break;
+            }
+
+        }
+
+        return answer;
+    }
+
+}
