0e2f3b4276c3f332f71655e8c4408b43f19cf78c
maenguin <maenguin924@gmail.com>
2020-12-22 11:02:12 +0900

    programmers x만큼간격이있는n개의숫자 solved

---
+++ src/main/java/programmers/level1/x만큼간격이있는n개의숫자.java
@@ -0,0 +1,15 @@
+package programmers.level1;
+
+public class x만큼간격이있는n개의숫자 {
+
+    public long[] solution(int x, int n) {
+        long[] answer = new long[n];
+
+        for (int i = 0; i < n; i++) {
+            answer[i] = (long) x * (i+1);
+        }
+        return answer;
+    }
+
+
+}
