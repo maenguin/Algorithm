cda1f20501ed2e0df5eb76c9c66ca69f99d3a2b5
maenguin <maenguin924@gmail.com>
2021-01-08 16:18:04 +0900

    programmers 가장큰수 solved

--- src/main/java/programmers/level2/가장큰수.java
+++ src/main/java/programmers/level2/가장큰수.java
@@ -1,4 +1,36 @@
 package programmers.level2;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class 가장큰수 {
+
+    public String solution(int[] numbers) {
+        String answer = "";
+
+        List<String> list = Arrays.stream(numbers).mapToObj(String::valueOf).collect(Collectors.toList());
+        Collections.sort(list, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+
+                if (o1.length() != o2.length()) {
+                    int min = Math.min(o1.length(), o2.length());
+                    if (o1.substring(0, min).equals(o2.substring(0, min))) {
+                        return (o2 + o1).compareTo(o1 + o2);
+                    }
+                }
+                return o2.compareTo(o1);
+                //return o1.compareTo(o2); 도 가능
+            }
+        });
+
+        StringBuilder builder = new StringBuilder();
+        for (String s : list) {
+            builder.append(s);
+        }
+        return builder.toString().replaceAll("^0*$","0");
+    }
+
+
+
 }
