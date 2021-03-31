c5150455b227b43de919ba97076654eb428f9301
maenguin <maenguin924@gmail.com>
2021-03-31 21:54:50 +0900

    baekJoon 나머지 solved

---
+++ src/main/java/baekJoon/나머지.java
@@ -0,0 +1,21 @@
+package baekJoon;
+
+import java.util.Scanner;
+
+//https://www.acmicpc.net/problem/10430
+public class 나머지 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+
+        System.out.println((a+b)%c);
+        System.out.println(((a%c)+(b%c))%c);
+        System.out.println((a*b)%c);
+        System.out.println(((a%c)*(b%c))%c);
+    }
+
+
+}
