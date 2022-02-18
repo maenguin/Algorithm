ae377d4752347a30739d28ca4da7bea9169c1f4c
maenguin <maenguin924@gmail.com>
2022-02-18 13:33:08 +0900

    add solution : baekJoon/P1149_RGB거리 bottom-up 방식

--- src/main/java/baekJoon/P1149_RGB거리.java
+++ src/main/java/baekJoon/P1149_RGB거리.java
@@ -13,7 +13,7 @@ public class P1149_RGB거리 {
     private static int ANSWER = (int) 1e9;
     private static int[][] DP;
 
-    public static void main(String[] args) {
+    public static void main2(String[] args) {
         Scanner sc = new Scanner(System.in);
         N = sc.nextInt();
         RGBS = new int[N][3];
@@ -40,5 +40,31 @@ public class P1149_RGB거리 {
         int b = dpWithDFS(n - 1, (rgb + 2) % 3);
         return DP[n][rgb] = RGBS[n][rgb] + Math.min(a, b);
     }
+
+    ////////////////////////////////////////////////////////
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        RGBS = new int[N][3];
+        for (int i = 0; i < N; i++) {
+            RGBS[i][0] = sc.nextInt();
+            RGBS[i][1] = sc.nextInt();
+            RGBS[i][2] = sc.nextInt();
+        }
+        DP = new int[N][3];
+        DP[0][0] = RGBS[0][0];
+        DP[0][1] = RGBS[0][1];
+        DP[0][2] = RGBS[0][2];
+        for (int i = 1; i < N; i++) {
+            DP[i][0] = RGBS[i][0] + Math.min(DP[i - 1][1], DP[i - 1][2]);
+            DP[i][1] = RGBS[i][1] + Math.min(DP[i - 1][0], DP[i - 1][2]);
+            DP[i][2] = RGBS[i][2] + Math.min(DP[i - 1][0], DP[i - 1][1]);
+        }
+        ANSWER = Math.min(DP[N-1][0], DP[N-1][1]);
+        ANSWER = Math.min(ANSWER, DP[N-1][2]);
+        System.out.println(ANSWER);
+    }
+
 
 }
